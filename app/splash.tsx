943615fdbc5218e953eeccdbe526874caf091de7
Kamen Kanchev <45595147+skyllex94@users.noreply.github.com>
2025-02-04 07:37:11 +0200

    splash screen

---
+++ app/splash.tsx
@@ -0,0 +1,18 @@
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+
+export default function SplashScreen() {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#ffffff" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
